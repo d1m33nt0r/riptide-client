fd32ef77d0336c35994dc14fbc0df1344f1185d7
dmytrohryb <dmitrii.9rib@gmail.com>
2022-12-25 14:49:52 +0200

    no message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,23 @@ public class Player : MonoBehaviour
     public ushort ID { get; private set; }
     public bool IsLocal { get; private set; }
 
+    [SerializeField] private Transform cameraTransform;
+    
     private string username;
 
     private void OnDestroy()
     {
         list.Remove(ID);
+    }
+
+    private void Move(Vector3 newPosition, Vector3 forward)
+    {
+        transform.position = newPosition;
+
+        if (!IsLocal)
+        {
+            cameraTransform.forward = forward;
+        }
     }
 
     public static void Spawn(ushort ID, string username, Vector3 position)
@@ -41,9 +53,21 @@ public class Player : MonoBehaviour
         list.Add(ID, player);
     }
 
+    #region Messages
+
     [MessageHandler((ushort)ServerToClientID.playerSpawned)]
     private static void SpawnPlayer(Message message)
     {
         Spawn(message.GetUShort(), message.GetString(), message.GetVector3());
     }
+    
+    [MessageHandler((ushort)ServerToClientID.playerMovement)]
+    private static void PlayerMovement(Message message)
+    {
+        if (list.TryGetValue(message.GetUShort(), out Player player))
+            player.Move(message.GetVector3(), message.GetVector3());
+    }
+
+    #endregion
+    
 }
